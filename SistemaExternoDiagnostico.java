9e02b024d7ec54b05f3de59516a9c00936fafefd
PabloUQ21 <juanp.londonog@uqvirtual.edu.co>
2025-10-15 09:08:41 -0500

    SistemaExternoDiagnostico.java

---
+++ SistemaExternoDiagnostico.java
@@ -0,0 +1,8 @@
+public class SistemaExternoDiagnostico {
+
+    public void ejecutarRevision(String modelo) {
+        System.out.println("🧩 [Sistema Externo] Ejecutando diagnóstico avanzado del modelo: " + modelo);
+        System.out.println("→ Verificando motor, frenos, luces y emisiones...");
+        System.out.println("→ Resultados: todo en orden ✅\n");
+    }
+}
